643871b3eba16f9f8ce2443eadf491dcef634a68
jordimc31 <jordiupc87@gmail.com>
2015-07-09 02:38:15 +0200

    Hello MadNums

---
+++ MadNums/Assets/Scripts/ScreenAdjust.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenAdjust : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	//Camera.main.aspect = Screen.width / Screen.height;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
