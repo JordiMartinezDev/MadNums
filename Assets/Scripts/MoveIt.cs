d8279f81c8818a164d9a9ed8a8a78d21b27922ee
jordimc31 <jordiupc87@gmail.com>
2015-07-13 05:05:59 +0200

    Movement Test

---
+++ Assets/Scripts/MoveIt.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveIt : MonoBehaviour {
+	public float speed;
+	bool right;
+	bool left;
+	// Use this for initialization
+	void Start () {
+	
+		right = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.x > 200) 
+		{
+			left = true;
+			right = false;
+		}
+		if (transform.position.x < -200)
+		{
+			left = false;
+			right = true;
+		}
+		if( right ) transform.Translate (new Vector3(1,0,0) * speed *  Time.deltaTime);
+		if( left ) transform.Translate (new Vector3(-1,0,0) * speed *  Time.deltaTime);
+	}
+	void FixedUpdate(){
+		//transform.Translate (new Vector3 ( 10,0,0));
+	}
+	
+}
