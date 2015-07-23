34b54af7c4e8fc89d93197e7fa590fdc52af023f
jordimc31 <jordiupc87@gmail.com>
2015-07-23 02:33:16 +0200

    sprites for level arrangement I

---
+++ Assets/Scripts/sprites.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class sprites {
+
+	public Sprite one;
+	public Sprite two;
+	public Sprite three;
+	public Sprite four;
+
+	GameObject[] Buttons;
+
+}
